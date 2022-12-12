1e340030dc3e18011b3c45e3512d1fb7f9557613
DAISI FAVOUR daisyfav196 <daisifavour87@gmail.com>
2022-12-12 03:29:08 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-prints.c
@@ -0,0 +1,11 @@
+#include<stdhio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("\" Program is a like building a multilingual puzzle");
+	return (0);
+}
